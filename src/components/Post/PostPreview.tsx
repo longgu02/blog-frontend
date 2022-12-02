07dc424c54f195e744c2f189f39def6254f76a9c
longgu02 <phamlong121120022@gmail.com>
2022-12-02 22:03:47 +0700

    chore: post preview

--- src/components/Post/PostPreview.tsx
+++ src/components/Post/PostPreview.tsx
@@ -1,5 +1,7 @@
 import { Typography, Paper, Box, Avatar } from '@mui/material'
 import Image from 'next/image'
+import moment from 'moment'
+import Link from 'next/link'
 
 interface Post {
   _id: string
@@ -18,10 +20,12 @@ interface Post {
 }
 
 export default function PostPreview(props: { key: Post['_id']; post: Post }) {
-  const { post } = props
+  const { post, key } = props
   const testImg =
     'https://images.pexels.com/photos/1519088/pexels-photo-1519088.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
   return (
+    <Link href={`/post/${post.slug}`}>
+    
     <Paper
       elevation={1}
       sx={{
@@ -32,7 +36,7 @@ export default function PostPreview(props: { key: Post['_id']; post: Post }) {
         justifyContent: 'space-between',
       }}
     >
-      <Box sx={{ pr: 1, pl: 2 }}>
+      <Box sx={{ pr: 1, pl: 2, position: 'relative' }}>
         <Typography
           variant="subtitle1"
           sx={{ maxWidth: '350px', fontWeight: 'bold', pb: 1 }}
@@ -41,11 +45,34 @@ export default function PostPreview(props: { key: Post['_id']; post: Post }) {
         </Typography>
         <Typography
           variant="body1"
-          sx={{ maxWidth: '400px', color: '	#A9A9A9', fontWeight: 'bold' }}
+          sx={{
+            maxWidth: '400px',
+            color: '	#A9A9A9',
+            fontWeight: 'bold',
+            mb: 1,
+          }}
         >
           {post.summary}
         </Typography>
-        <Avatar sx={{}} />
+        {post.user && (
+          <Box
+            sx={{ flexWrap: 'wrap', display: 'flex', justifyContent: 'start', mt: 'auto', mb: 0, position: 'absolute', bottom: 10 }}
+          >
+            <Avatar sx={{ width: 30, height: 30 }} />
+            <Typography
+              variant="body2"
+              sx={{ margin: 'auto 0 auto 5px', fontWeight: 'bold' }}
+            >
+              {`${post.user?.firstName} ${post.user?.lastName} - `}
+            </Typography>
+            <Typography
+              variant="body2"
+              sx={{ margin: 'auto 0 auto 5px', fontWeight: 'bold' }}
+            >
+              {moment(post.createAt).format('MMM Do YY')}
+            </Typography>
+          </Box>
+        )}
       </Box>
       <Image
         src={testImg}
@@ -55,5 +82,6 @@ export default function PostPreview(props: { key: Post['_id']; post: Post }) {
         alt="Thumbnail"
       ></Image>
     </Paper>
+    </Link>
   )
 }
